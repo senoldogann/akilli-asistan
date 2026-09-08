ab21746ef97fe21ab00b33829e5e29d1de08fad5
Senol Dogan <s.dogann@yandex.com>
2026-09-08 12:31:16 +0300

    test(exampilot): scaffold Swift package for red tests

---
+++ ExamPilot/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "ExamPilot",
+    platforms: [.macOS(.v14)],
+    products: [
+        .library(name: "ExamPilotCore", targets: ["ExamPilotCore"]),
+        .executable(name: "exampilot", targets: ["ExamPilotCLI"]),
+    ],
+    targets: [
+        .target(name: "ExamPilotCore"),
+        .executableTarget(name: "ExamPilotCLI", dependencies: ["ExamPilotCore"]),
+        .testTarget(name: "ExamPilotCoreTests", dependencies: ["ExamPilotCore"]),
+    ]
+)
